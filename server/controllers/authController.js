cd51ca1b60022b9949c9a60da0031fec54c5cffb
Gayatri414 <gayatrigade7949@gmail.com>
2026-07-09 02:03:09 +0530

    add file email.js

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -2,10 +2,21 @@ const User=require('../models/User');
 
 exports.registerUser=async(req,res)=>{
     const{name,email,password}=req.body;
+    let userExists=await User.findOne({email});
+    if(userExists){
+        return res.status(400).json({error:'User already exists'});
+    }
+const salt=await bcrypt.genSalt(10);
+const hashedPassword=await bcrypt.hash(password,salt);
+
+
     try{
         const user=new User({name,email,password});
         await user.save();
         res.status(201).json({message:'User registered successfully'});
+
+        const otp=Math.floor(10000+Math.random()*900000).toString();
+        console.log(`OTP for ${email}:${otp}`);
 
     }catch(error){
         res.status(400).json({error:error.message});
