64f4ee4daaa68b738727fc6caf623b3654c82374
Gayatri414 <gayatrigade7949@gmail.com>
2026-07-14 19:32:16 +0530

    add changes

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -1,4 +1,5 @@
-const User=require('../models/User');
+const User = require('../models/User');
+const OTP = require('../models/OTP');
 
 exports.registerUser=async(req,res)=>{
     const{name,email,password}=req.body;
@@ -7,16 +8,32 @@ exports.registerUser=async(req,res)=>{
         return res.status(400).json({error:'User already exists'});
     }
 const salt=await bcrypt.genSalt(10);
-const hashedPassword=await bcrypt.hash(password,salt);
+const hashedPassworconst User = require('../models/User.js');
+const OTP = require('../models/OTP');
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const { sendOTPEmail } = require('../utils/email');
+
+const generateOTP = () => Math.floor(100000 + Math.random() * 900000).toString();
+
+const generateToken = (id, role) => {
+    return jwt.sign({ id, role }, process.env.JWT_SECRET, { expiresIn: '30d' });
+};
+
+exports.register = async (req, res) => {
+};
+d=await bcrypt.hash(password,salt);
 
 
     try{
-        const user=new User({name,email,password});
-        await user.save();
+        const user=new User.create({name,email,password:hashedPassword,roler:'user',isVerified:false});
+    
         res.status(201).json({message:'User registered successfully'});
 
         const otp=Math.floor(10000+Math.random()*900000).toString();
         console.log(`OTP for ${email}:${otp}`);
+await sendOTPEmail(email,otp,'account_verification');
+
 
     }catch(error){
         res.status(400).json({error:error.message});
